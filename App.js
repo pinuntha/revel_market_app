d5a3b3140ef141f1abc50bcdb72a9c64affff0d8
pinuntha <60280341+pinuntha@users.noreply.github.com>
2020-01-25 11:59:57 +0700

    revel_market_app

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Login } from './views/Login/Login'
- 
-  const App = () => {
-    return(
-      <Login btn="Hello World"/>
-    );
-  };
-export default App;
+import { Home } from './views/Home/Home'
 
+const App = () => {
+  return (
+    <Home/>
+  );
+};
+
+export default App;
---
+++ GOBALS.js
@@ -0,0 +1,3 @@
+export default {
+	URL: 'http://191.1.2.170:5001/',
+}
